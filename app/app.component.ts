9398e8411820a0c8cf7c27c20a25f4836c231a14
OneOfOne <oneofone@gmail.com>
2016-07-10 21:03:44 +0200

    initial routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { provideRouter, RouterConfig, ROUTER_DIRECTIVES } from '@angular/router';
 import { SignInComponent } from './signin.component';
+
+export const routes: RouterConfig = [
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full'
+	},
+	// {
+	// 	path: 'dashboard',
+	// 	component: DashboardComponent
+	// },
+	{
+		path: 'signIn',
+		component: SignInComponent
+	},
+];
+
+export const ALL_ROUTES = [provideRouter(routes)];
 
 @Component({
 	selector: 'sway-app',
-	template : `
-		<signIn></signIn>
-	`,
-	directives: [SignInComponent]
+	template : `<router-outlet></router-outlet>`,
+	directives: [ROUTER_DIRECTIVES],
+	precompile: [routes.map(r => r.component).filter(r => !!r)] // dynamically precompile all used route components
 })
 
-export class AppComponent {
-}
+export class AppComponent {}
--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,17 @@
 import 'rxjs/Rx';
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
-
 import { HTTP_PROVIDERS } from '@angular/http';
-import { AppComponent } from './app.component';
 
+import { ALL_ROUTES, AppComponent } from './app.component';
+import { SignInComponent } from './signin.component';
+
+
+
+enableProdMode();
 bootstrap(AppComponent, [
+	ALL_ROUTES,
 	disableDeprecatedForms(),
 	provideForms(),
 	HTTP_PROVIDERS
