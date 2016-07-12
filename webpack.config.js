0cc84f81952f5b1abe8e36c37b42a72b93356b4d
OneOfOne <oneofone@gmail.com>
2016-07-12 22:00:41 +0200

    make incremental compiles faster

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,41 @@
 'use strict';
 var webpack = require('webpack'),
-	ExtractTextPlugin = require('extract-text-webpack-plugin'),
+	path = require('path'),
+	fs = require('fs'),
 	CompressionPlugin = require("compression-webpack-plugin");
+
+// clean up old files
+['app.js', 'app.js.map', 'app.js.gz', 'vendor.js', 'vendor.js.map', 'vendor.js.gz'].forEach(function(fp) {
+	// eslint-disable-next-line
+	try { fs.unlinkSync('static/' + fp); } catch (e) { };
+});
 
 const ENV = process.env.ENV || 'dev';
 
-var entries = {
-	//	'vendor': './app/vendor.ts',
-	'app': './app/main.ts'
-},
-	plugins = [
-		new webpack.NoErrorsPlugin(),
+var cfg = {
+	devtool: 'eval',
 
-		new webpack.DefinePlugin({
-			'process.env': {
-				'ENV': JSON.stringify(ENV)
-			}
-		})
-	];
+	entry: {
+		'vendor': [
+			'es6-shim', 'reflect-metadata', 'zone.js/dist/zone', 'rxjs', '@angular/platform-browser',
+			'@angular/platform-browser-dynamic', '@angular/core', '@angular/common', '@angular/compiler',
+			'@angular/router', '@angular/http', '@angular/forms'
+		],
+		//'vendor': './app/vendor.ts',
+		'app': './app/main.ts'
 
-if (ENV === 'production') {
-	plugins = plugins.concat([
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: Object.keys(entries)
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				dead_code: true,
-				properties: true,
-				drop_debugger: true,
-				evaluate: true,
-				loops: true,
-				unused: true,
-				join_vars: true,
-				warnings: false,
-				passes: 2
-			},
-			'screw-ie8': true,
-		}),
-
-		new CompressionPlugin({
-			asset: "[path].gz[query]",
-			algorithm: "gzip",
-			test: /\.js$|\.css$|\.html$/,
-			threshold: 10240,
-			minRatio: 0.8
-		})
-	]);
-}
-
-module.exports = {
-	devtool: ENV === 'production' ? 'source-map' : 'eval',
-	entry: entries,
-	output: {
-		filename: 'static/[name].js'
 	},
+
+	output: {
+		filename: 'static/[name].js',
+		chunkFilename: 'static/[name].js'
+	},
+
 	resolve: {
+		root: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'app')],
 		extensions: ['', '.js', '.ts']
 	},
-	htmlLoader: {
-		minimize: false // workaround for ng2
-	},
+
 	module: {
 		loaders: [
 			{
@@ -73,24 +47,60 @@ module.exports = {
 				loader: 'raw-loader',
 				//exclude: root('.'),
 			},
-			{
-				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-				loader: 'file?name=assets/[name].[hash].[ext]'
-			},
-			{
-				test: /\.css$/,
-				//exclude: helpers.root('src', 'app'),
-				loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-			},
-			{
-				test: /\.css$/,
-				//include: helpers.root('src', 'app'),
-				loader: 'raw'
-			}
 		]
 	},
 
-	plugins: plugins
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'ENV': JSON.stringify(ENV)
+			}
+		}),
+		new webpack.PrefetchPlugin('es6-shim', 'reflect-metadata', 'zone.js/dist/zone', 'rxjs',
+			'@angular/platform-browser', '@angular/platform-browser-dynamic', '@angular/core', '@angular/common',
+			'@angular/router', '@angular/http', '@angular/forms'),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: Infinity,
+		}),
+	]
 };
+
+if (ENV === 'production') {
+	cfg.devtool = 'source-map';
+	cfg.plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				dead_code: true,
+				properties: true,
+				drop_debugger: true,
+				evaluate: true,
+				loops: true,
+				unused: true,
+				warnings: false,
+				sequences: true,
+				conditionals: true,
+				booleans: true,
+				if_return: true,
+				join_vars: true,
+				drop_console: true,
+				passes: 2
+			},
+			'screw-ie8': true,
+		}),
+
+		new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.js$|\.css$|\.html$/,
+			threshold: 4096,
+			minRatio: 0.8
+		})
+	);
+}
+
+module.exports = cfg;
 
 console.log('Build ENV:', ENV);
