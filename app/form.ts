3ad8201a03f1d1881fb4c166ec6c10f7821d9af9
OneOfOne <oneofone@gmail.com>
2016-09-02 22:33:31 +0200

    improve form handling and allow sub-object binding to make life easier

--- app/form.ts
+++ app/form.ts
@@ -21,7 +21,7 @@ export class Form {
 	};
 
 	private loading = false;
-
+	private binders = {};
 	constructor() {}
 
 	save(f: NgForm) {
@@ -33,6 +33,12 @@ export class Form {
 		this.onSave(this.data, () => this.loading = false);
 	}
 
+	bind(fld: ControlOptions): Binder {
+		if(!this.binders[fld.name]) {
+			this.binders[fld.name] = new Binder(this.data, fld);
+		}
+		return this.binders[fld.name];
+	}
 
 	validate(f: NgForm, fld: ControlOptions) {
 		const ctl = f.form.controls[fld.name],
@@ -48,7 +54,15 @@ export class Form {
 			errors[fld.error ? fld.error : 'Value mismatch'] = true;
 		}
 
-		ctl.setErrors(Object.keys(errors).length > 0 ? errors : null)
+		fld.valid = Object.keys(errors).length === 0
+		ctl.setErrors(fld.valid ? null : errors)
+	}
+
+	get valid(): boolean {
+		if(this.loading) {
+			return false;
+		}
+		return this.fields.filter(v => v.req && v.valid).length === 0
 	}
 
 	errors(f: NgForm, fld: ControlOptions) {
@@ -74,4 +88,26 @@ export interface ControlOptions {
 	input?: string;
 	textarea?: boolean;
 	checkbox?: boolean;
+
+	valid?: boolean;
+}
+
+class Binder {
+	private name: string;
+	constructor(private data: Object, private fld: ControlOptions) {
+		let parts = fld.name.split('.'),
+			lastKey = parts[parts.length - 1];
+		for(let i = 0; i < parts.length - 1; i++) {
+			const k = parts[i];
+			data = (data[k] = data[k] || {});
+		}
+		this.data = data;
+		this.name = lastKey;
+	}
+
+	set value(val: any) {
+		if(this.fld.input === 'number') val = parseFloat(val);
+		this.data[this.name] = val;
+	}
+	get value(): any { return this.data[this.name]; }
 }
