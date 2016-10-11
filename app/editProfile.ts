5862e4f12305bb4d76b9ba552d59479e19e33a32
OneOfOne <oneofone@gmail.com>
2016-10-11 19:08:29 +0200

    show the current profile picture

--- app/editProfile.ts
+++ app/editProfile.ts
@@ -17,8 +17,7 @@ export class EditProfileCmp extends ManageBase {
 
 	public fields: any[] = [
 		{
-			title: 'Profile Pic:', placeholder: 'Your profile pic', image: true, name: 'pic',
-			attrs: { accept: 'image/*' },
+			title: 'Profile Pic:', placeholder: 'Your profile pic', image: true, name: 'imageUrl',
 		},
 		{
 			title: 'Account Name:', placeholder: 'Your brand or name', input: 'text', name: 'name', req: true,
