849d1a7e29d870eb624f175c8246217936e93301
OneOfOne <oneofone@gmail.com>
2016-10-27 22:19:40 +0200

    actually make the sent button do something

--- app/mPerks.ts
+++ app/mPerks.ts
@@ -27,6 +27,7 @@ export class CampaignPerksCmp extends ManageBase {
 	templateUrl: './views/mOutboundPerks.html',
 })
 export class OutboundPerksCmp extends ManageBase {
+	public loading = false;
 	constructor(title: Title, api: Sway) {
 		super('getPendingPerks', 'Outbound Perks', title, api);
 	}
@@ -38,6 +39,19 @@ export class OutboundPerksCmp extends ManageBase {
 			addr.address_line1, addr.address_line2, addr.address_city, addr.address_state,
 			addr.address_zip, addr.address_country,
 		].filter(v => !!v && v.length).join(', ');
+	}
+
+	Sent(cmpID: string, infID: string) {
+		this.loading = true;
+		this.api.Get('approvePerk/' + infID + '/' + cmpID, resp => {
+			this.loading = false;
+			this.Reload();
+			this.AddNotification('success', 'Approved!', 5000);
+			this.ScrollToTop();
+		}, err => {
+			this.loading = false;
+			this.AddNotification('error', err.msg);
+		});
 	}
 }
 
