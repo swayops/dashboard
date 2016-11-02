3be70a8fd2a7e541fffcec88c893371e1e16744f
OneOfOne <oneofone@gmail.com>
2016-11-02 17:16:33 +0200

    fix the clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,16 @@ function CleanPlugin(base, files) {
 }
 
 CleanPlugin.prototype = {
-	allFiles: function() { return this.files.concat(this.files.map(fp => this.exts.map(ext => fp + ext))); },
+	allFiles: function() {
+		const out = [];
+		for (let fp of this.files) {
+			out.push(fp);
+			for (let ext of this.exts) {
+				out.push(fp + ext);
+			}
+		}
+		return out;
+	},
 
 	apply: function(compiler) {
 		compiler.plugin('done', () => this.clean());
