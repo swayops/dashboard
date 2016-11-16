44e72e658273fd30043251ce71f057f2e7f582fe
OneOfOne <oneofone@gmail.com>
2016-11-16 21:33:31 +0200

    handle errors cleanly for mBilling

--- app/mBilling.ts
+++ app/mBilling.ts
@@ -13,45 +13,63 @@ import { Pad } from './utils';
 })
 export class ManageBillingCmp extends ManageBase {
 	public cc: any = {};
+	public activeBalance = 0;
+	public inactiveBalance = 0;
 	public isEditing = false;
 	public history: any[];
+	public loading = false;
 	constructor(title: Title, public api: Sway, route: ActivatedRoute) {
 		super('billingInfo', 'Billing', title, api, route.snapshot.params['id'], resp => {
-			this.list = [];
+			this.list = null;
 			if (!resp || !resp.cc || !resp.cc.cardNumber) {
 				this.cc.num = ['', '', '', ''];
 				this.isEditing = true;
 				return;
 			}
-			this.cc = resp.cc;
+			this.cc = Object.assign({}, resp.cc);
+			this.activeBalance = resp.activeBalance || 0;
+			this.inactiveBalance = resp.inactiveBalance || 0;
 			this.formatCC(this.cc);
 			this.history = resp.history;
 		});
 	}
 
 	save(f: any) {
-		console.log(f);
+		this.loading = true;
 		const adv = Object.assign({}, this.user.advertiser);
 		adv.ccLoad = Object.assign({}, this.cc);
 		this.formatCC(adv.ccLoad);
 		delete adv.blacklist;
 		this.api.Put('advertiser/' + this.id, { advertiser: adv }, resp => {
-			console.log(resp);
-		}, err => console.log(err));
+			this.loading = false;
+			this.isEditing = false;
+			if (resp.status === 'success') {
+				this.AddNotification(resp.status, 'Successfully updated your credit card information.');
+			} else {
+				this.AddNotification(resp.status, this.getErrorMsg(resp));
+			}
+			this.ScrollToTop();
+		}, err => {
+			this.AddNotification('error', this.getErrorMsg(err), 0);
+			this.ScrollToTop();
+			this.loading = false;
+		});
 	}
 
 	private formatCC(cc: any) {
 		if (cc.expMonth) {
 			cc.expMonth = Pad(parseInt(cc.expMonth));
 		}
-		if (typeof cc.expYear === 'number') cc.expYear = cc.expYear.toString();
 		if (typeof cc.cvc === 'number') cc.cvc = cc.cvc.toString();
 		if (cc.num) {
 			cc.cardNumber = cc.num.join('-');
+			cc.expYear = (parseInt(cc.expYear) - 2000).toString();
 			delete cc.num;
 		} else {
 			cc.num = ['', '', '', cc.cardNumber || ''];
+			cc.expYear = (parseInt(cc.expYear) + 2000).toString();
 		}
+		if (typeof cc.expYear === 'number') cc.expYear = cc.expYear.toString();
 		console.log(cc);
 	}
 
@@ -60,5 +78,14 @@ export class ManageBillingCmp extends ManageBase {
 		const val = (<HTMLInputElement> evt.target).value;
 		if (key < 0 || key > 9 || val.length < 4) return;
 		next.focus();
+	}
+
+	getErrorMsg(resp: any): string {
+		console.log(resp.msg, resp.msg.substr(0, 1));
+		if (resp.msg.substr(0, 1) === '{') {
+			const rerr = JSON.parse(resp.msg) || { message: 'Unknown error' };
+			return rerr.message;
+		}
+		return resp.msg;
 	}
 }
