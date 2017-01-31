9dcd125bf2726c8258fe12a4839e26964dccca77
OneOfOne <oneofone@gmail.com>
2017-01-31 02:14:26 +0200

    all the timelines are getting mixed Jim, life as we know it will end.

--- app/reporting.ts
+++ app/reporting.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 import { Sway } from './sway';
 
@@ -16,6 +16,7 @@ export class ReportingCmp {
 	public curWeek: any = {};
 	public lastWeek: any = {};
 	public lastMonth: any = {};
+	public timelines: Timeline[];
 	private id;
 	constructor(title: Title, public api: Sway, route: ActivatedRoute) {
 		title.setTitle('Sway :: Reporting');
@@ -24,14 +25,15 @@ export class ReportingCmp {
 			console.error('bad id');
 			return;
 		}
-		api.SetCurrentUser(this.id).then(_ => {
-			this.api.Get('getAdvertiserStats/' + this.id + '/7/0', data => this.curWeek = data.total || {});
-			this.api.Get('getAdvertiserStats/' + this.id + '/14/7', data => this.lastWeek = data.total || {});
-			this.api.Get('getAdvertiserStats/' + this.id + '/30/0', data => {
+		api.SetCurrentUser(this.id).then((_) => {
+			this.api.Get('getAdvertiserStats/' + this.id + '/7/0', (data) => this.curWeek = data.total || {});
+			this.api.Get('getAdvertiserStats/' + this.id + '/14/7', (data) => this.lastWeek = data.total || {});
+			this.api.Get('getAdvertiserStats/' + this.id + '/30/0', (data) => {
 				this.lastMonth = data || {};
 				this.setGraph('engagements');
 			});
-		}).catch(err => {
+			this.api.Get('getAdvertiserTimeline/' + this.id, (data) => this.setTimeline(data || {}));
+		}).catch((err) => {
 			api.Logout();
 		});
 	}
@@ -39,10 +41,39 @@ export class ReportingCmp {
 	setGraph(key) {
 		try {
 			const data = this.lastMonth;
-			initChartData(Object.keys(data).map(k => {
+			initChartData(Object.keys(data).map((k) => {
 				if (k === 'total') return null;
 				return { date: k, value: data[k][key] || 0 };
-			}).filter(v => !!v));
+			}).filter((v) => !!v));
 		} catch (e) { console.error(e); }
 	}
+
+	private setTimeline(data: { [key: string]: Timeline[] }) {
+		const out = new Array<Timeline>();
+		for (const [k, v] of Object.entries(data)) {
+			let i = 0;
+			for (const tl of v) {
+				tl.title = k;
+				tl.ts = (tl.ts * 1000) + i; // js date fix and sorting
+				out.push(tl);
+				i++;
+			}
+		}
+		out.sort((a, b) => {
+			if (!a) return -1;
+			if (!b) return 1;
+			if (a.ts < b.ts) return -1;
+			if (a.ts > b.ts) return 1;
+			return 0;
+		});
+		this.timelines = out;
+	}
+}
+
+
+interface Timeline {
+	title: string;
+	msg: string;
+	ts: number;
+	link?: string;
 }
