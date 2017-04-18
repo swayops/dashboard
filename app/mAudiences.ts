cc33166609d6db1f52ee49f320655e4a762d8d2b
OneOfOne <oneofone@gmail.com>
2017-04-18 22:57:54 +0200

    delete support

--- app/mAudiences.ts
+++ app/mAudiences.ts
@@ -14,15 +14,28 @@ declare const $: any;
 })
 export class AudiencesCmp extends ManageBase {
 	constructor(title: Title, api: Sway, route: ActivatedRoute) {
-		super('audience', 'Audiences', title, api, null, () => {
-			if (Array.isArray(this.list)) return;
-			const list = [];
-			for (const k of Object.keys(this.list).sort()) {
-				const o = this.list[k];
-				o.sub = Object.keys(o.members).length;
-				list.push(o);
-			}
-			this.list = list;
+		super('audience', 'Audiences', title, api, null, () => this.init());
+	}
+	private init() {
+		if (Array.isArray(this.list)) return;
+		const list = [];
+		for (const k of Object.keys(this.list).sort()) {
+			const o = this.list[k];
+			o.sub = Object.keys(o.members).length;
+			list.push(o);
+		}
+		this.list = list;
+	}
+
+	Delete(id: string) {
+		this.loading = true;
+		this.api.Delete('audience/' + id, (resp) => {
+			this.loading = false;
+			this.AddNotification(resp.status, resp.status === 'success' ? 'Successfully Removed Audience.' : resp.msg, 5000);
+			this.Reload(() => this.init());
+		}, (err) => {
+			this.AddNotification('error', err, 5000);
+			this.loading = false;
 		});
 	}
 }
